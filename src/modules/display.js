c0ce1cb5a292407b02210c2f30722f7f2cad9e1e
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-09-07 21:23:06 -0400

    finished weather-main layout

--- src/modules/display.js
+++ src/modules/display.js
@@ -2,15 +2,14 @@
 export default async function displayWeather(weatherResponse) {
   const weatherObj = await weatherResponse;
 
-  const location = document.querySelector(".location");
-  const condition = document.querySelector(".condition");
-  const conditionIcon = document.querySelector("#condition-img");
-  const temp = document.querySelector(".temp");
-  const humidity = document.querySelector(".humidity");
+  const location = document.querySelector("#location");
+  const condition = document.querySelector("#condition");
+  const conditionIcon = document.querySelector("#condition-icon");
+  const temp = document.querySelector("#temp");
 
   location.textContent = weatherObj.loc;
   condition.textContent = `${weatherObj.cond}`;
   conditionIcon.setAttribute("src", weatherObj.condIcon);
-  temp.textContent = `${weatherObj.tempF}°F / ${weatherObj.tempC}°C`;
+  temp.textContent = `${weatherObj.tempF}°F`;
   humidity.textContent = `${weatherObj.humid}%`;
 }
