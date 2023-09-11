1ebf807db76dc6d76ab695e03ffde76377bc9aff
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-09-10 23:50:58 -0400

    added toggle for temp unit

--- modules/display.js
+++ modules/display.js
@@ -1,33 +1,43 @@
-// receives a promise resolving in a a weather object
-export default async function displayWeather(weatherResponse) {
+// resolves [weatherResponse] then displays information on webpage
+export default async function displayWeather(weatherResponse, tUnit) {
   const weatherObj = await weatherResponse;
 
   // display main info
   const location = document.querySelector("#location");
-  const condition = document.querySelector("#condition");
-  const temp = document.querySelector("#temp");
-  const tempHi = document.querySelector("#temp-hi");
-  const tempLo = document.querySelector("#temp-lo");
-
   location.textContent = weatherObj.mainInfo.loc;
-  temp.textContent = `${weatherObj.mainInfo.tempF}°`;
-  condition.textContent = `${weatherObj.mainInfo.cond}`;
-  tempHi.textContent = `H:${weatherObj.mainInfo.tempHiF}°`;
-  tempLo.textContent = `L:${weatherObj.mainInfo.tempLoF}°`;
+
+  displayTemp(weatherObj, tUnit);
 
   // display extra details
-
-  const tempFeel = document.querySelector("#temp-feel");
   const humid = document.querySelector("#humid");
   const uv = document.querySelector("#uv");
   const wind = document.querySelector("#wind");
   const vis = document.querySelector("#vis");
   const pressure = document.querySelector("#pressure");
 
-  tempFeel.innerHTML = `<h3>Feels like</h3> ${weatherObj.extraInfo.tempFeelF}°`;
   humid.innerHTML = `<h3>Humidity</h3> ${weatherObj.extraInfo.humid}%`;
   uv.innerHTML = ` <h3>UV Index</h3> ${weatherObj.extraInfo.uv}`;
   wind.innerHTML = `<h3>Wind</h3> ${weatherObj.extraInfo.windSpd} mph ${weatherObj.extraInfo.windDir}`;
   vis.InnerHTML = `<h3>Visibility</h3> ${weatherObj.extraInfo.vis} mi`;
   pressure.innerHTML = `<h3>Pressure</h3> ${weatherObj.extraInfo.pressure} mb`;
+}
+
+// given populates temperature elements based on temperature unit
+function displayTemp(weatherObj, tUnit) {
+  const temp = document.querySelector("#temp");
+  const tempHi = document.querySelector("#temp-hi");
+  const tempLo = document.querySelector("#temp-lo");
+  const tempFeel = document.querySelector("#temp-feel");
+
+  if (tUnit == "c" || tUnit == "C") {
+    temp.textContent = `${weatherObj.mainInfo.tempC}°`;
+    tempHi.textContent = `H:${weatherObj.mainInfo.tempHiC}°`;
+    tempLo.textContent = `L:${weatherObj.mainInfo.tempLoC}°`;
+    tempFeel.innerHTML = `<h3>Feels like</h3> ${weatherObj.extraInfo.tempFeelC}°`;
+  } else {
+    temp.textContent = `${weatherObj.mainInfo.tempF}°`;
+    tempHi.textContent = `H:${weatherObj.mainInfo.tempHiF}°`;
+    tempLo.textContent = `L:${weatherObj.mainInfo.tempLoF}°`;
+    tempFeel.innerHTML = `<h3>Feels like</h3> ${weatherObj.extraInfo.tempFeelF}°`;
+  }
 }
--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 import getForecast from "./modules/weather.js";
 import displayWeather from "./modules/display.js";
 
+let data = null;
 getForecast("London")
-  .then((weatherObject) => {
-    console.log(weatherObject);
-    displayWeather(weatherObject);
+  .then((forecastObj) => {
+    data = forecastObj;
+    displayWeather(forecastObj, "f");
   })
   .catch(alert);
 
@@ -16,5 +17,34 @@ form.addEventListener("submit", (e) => {
   const input = document.querySelector("input");
   getForecast(input.value)
     .then((weatherObject) => displayWeather(weatherObject))
+    .catch(alert);
+});
+
+const fToggle = document.querySelector("#f-toggle");
+const cToggle = document.querySelector("#c-toggle");
+
+// change temp unit to F
+fToggle.addEventListener("click", (e) => {
+  e.preventDefault();
+  fToggle.classList.add("active");
+  cToggle.classList.remove("active");
+  getForecast("London")
+    .then((forecastObj) => {
+      data = forecastObj;
+      displayWeather(forecastObj, "f");
+    })
+    .catch(alert);
+});
+
+// change temp unit to C
+cToggle.addEventListener("click", (e) => {
+  e.preventDefault();
+  cToggle.classList.add("active");
+  fToggle.classList.remove("active");
+  getForecast("London")
+    .then((forecastObj) => {
+      data = forecastObj;
+      displayWeather(forecastObj, "c");
+    })
     .catch(alert);
 });
