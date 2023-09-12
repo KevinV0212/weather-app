3714898b8ff337fee1de7c48402a35ccc38c0b1c
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-09-12 17:21:33 -0400

    added icons

--- modules/display.js
+++ modules/display.js
@@ -15,11 +15,11 @@ export default async function displayWeather(weatherResponse, tUnit) {
   const vis = document.querySelector("#vis");
   const pressure = document.querySelector("#pressure");
 
-  humid.innerHTML = `<h3>Humidity</h3> ${weatherObj.extraInfo.humid}%`;
-  uv.innerHTML = ` <h3>UV Index</h3> ${weatherObj.extraInfo.uv}`;
-  wind.innerHTML = `<h3>Wind</h3> ${weatherObj.extraInfo.windSpd} mph ${weatherObj.extraInfo.windDir}`;
-  vis.InnerHTML = `<h3>Visibility</h3> ${weatherObj.extraInfo.vis} mi`;
-  pressure.innerHTML = `<h3>Pressure</h3> ${weatherObj.extraInfo.pressure} mb`;
+  humid.innerHTML = `<i class="icon wi wi-humidity"></i> <h3>Humidity</h3> ${weatherObj.extraInfo.humid}%`;
+  uv.innerHTML = ` <i class="icon wi wi-day-sunny"></i> <h3>UV Index</h3> ${weatherObj.extraInfo.uv}`;
+  wind.innerHTML = `<i class="icon wi wi-wind from-28-deg"></i> <h3>Wind</h3> ${weatherObj.extraInfo.windSpd} mph ${weatherObj.extraInfo.windDir}`;
+  vis.innerHTML = `<span class="icon mdi mdi-eye"></span> <h3>Visibility</h3> ${weatherObj.extraInfo.vis} mi`;
+  pressure.innerHTML = `<i class="icon wi wi-barometer"></i> <h3>Pressure</h3> ${weatherObj.extraInfo.pressure} mb`;
 }
 
 // given populates temperature elements based on temperature unit
@@ -33,11 +33,11 @@ function displayTemp(weatherObj, tUnit) {
     temp.textContent = `${weatherObj.mainInfo.tempC}°`;
     tempHi.textContent = `H:${weatherObj.mainInfo.tempHiC}°`;
     tempLo.textContent = `L:${weatherObj.mainInfo.tempLoC}°`;
-    tempFeel.innerHTML = `<h3>Feels like</h3> ${weatherObj.extraInfo.tempFeelC}°`;
+    tempFeel.innerHTML = `<i class="icon wi wi-thermometer"></i> <h3>Feels like</h3> ${weatherObj.extraInfo.tempFeelC}°`;
   } else {
     temp.textContent = `${weatherObj.mainInfo.tempF}°`;
     tempHi.textContent = `H:${weatherObj.mainInfo.tempHiF}°`;
     tempLo.textContent = `L:${weatherObj.mainInfo.tempLoF}°`;
-    tempFeel.innerHTML = `<h3>Feels like</h3> ${weatherObj.extraInfo.tempFeelF}°`;
+    tempFeel.innerHTML = `<i class="icon wi wi-thermometer"></i> <h3>Feels like</h3> ${weatherObj.extraInfo.tempFeelF}°`;
   }
 }
